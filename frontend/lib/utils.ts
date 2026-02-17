112f8b2c3910ccfa30225d66fbf42da911d8cb10
DevSolex <devsolex@users.noreply.github.com>
2026-02-17 10:07:44 +0100

    chore: clean utils.ts

--- frontend/lib/utils.ts
+++ frontend/lib/utils.ts
@@ -10,11 +10,3 @@ export function shortenAddress(addr: string, start = 6, end = 4): string {
 export function isValidPrincipal(addr: string): boolean {
   return /^S[PM2][0-9A-Z]{38}$/.test(addr);
 }
-// S-pay commit 23
-// S-pay commit 24
-// S-pay commit 25
-// S-pay commit 26
-// S-pay commit 27
-// S-pay commit 28
-// S-pay commit 29
-// S-pay commit 30
