32c1492a43f642e07cd2536c14bb7f37aa885370
DevSolex <devsolex@users.noreply.github.com>
2026-02-17 10:07:30 +0100

    chore: minor update (23)

--- frontend/lib/utils.ts
+++ frontend/lib/utils.ts
@@ -10,3 +10,4 @@ export function shortenAddress(addr: string, start = 6, end = 4): string {
 export function isValidPrincipal(addr: string): boolean {
   return /^S[PM2][0-9A-Z]{38}$/.test(addr);
 }
+// S-pay commit 23
